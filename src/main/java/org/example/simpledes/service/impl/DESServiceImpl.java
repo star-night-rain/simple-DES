4a8482a2d5000ad9afbec2ec8b56595b47c43711
star-night-rain <1747432970.@qq.com>
2024-09-27 18:09:20 +0800

    实现字符串的加密解密功能

--- src/main/java/org/example/simpledes/service/impl/DESServiceImpl.java
+++ src/main/java/org/example/simpledes/service/impl/DESServiceImpl.java
@@ -37,15 +37,57 @@ public class DESServiceImpl implements DESService {
         return desManager.finalPermutation(left + right);
     }
 
-    public String encode(EncodeDto encodeDto) {
-        String plainText = encodeDto.getPlainText();
-        String secretKey = encodeDto.getSecretKey();
+    public boolean valid(String str) {
+        return str.matches("[01]{8}");
+    }
+
+    public String transform(String originalText, String secretKey, int type) {
+        String modifiedText = "";
 
         String[] subKeys = desManager.secretKeyExpand(secretKey);
         String subKey1 = subKeys[0];
         String subKey2 = subKeys[1];
 
-        String cipherText = des(plainText, subKey1, subKey2);
+        //8bit
+        if (valid(originalText)) {
+            {
+                if (type == 1)
+                    modifiedText = des(originalText, subKey1, subKey2);
+                else
+                    modifiedText = des(originalText, subKey2, subKey1);
+            }
+        } else {
+            StringBuilder tentativeText = new StringBuilder();
+            for (char c : originalText.toCharArray()) {
+                String binaryNum = Integer.toBinaryString((int) c);
+                log.info("binaryNum:" + binaryNum);
+                String text = (binaryNum.length() >= 8) ? binaryNum.substring(0, 8) :
+                        String.format("%8s", binaryNum).replace(' ', '0');
+                log.info("text:" + text);
+                String cipherStr = "";
+                if (type == 1)
+                    cipherStr = des(text, subKey1, subKey2);
+                else
+                    cipherStr = des(text, subKey2, subKey1);
+                log.info("cipherStr:" + cipherStr);
+                int cipherNum = Integer.parseInt(cipherStr, 2);
+                log.info("cipherNum:" + cipherNum);
+                char cipherChar = (char) cipherNum;
+                log.info("cipherChar:" + cipherChar);
+                tentativeText.append(cipherChar);
+            }
+            modifiedText = tentativeText.toString();
+        }
+
+        return modifiedText;
+    }
+
+    public String encode(EncodeDto encodeDto) {
+        String plainText = encodeDto.getPlainText();
+        String secretKey = encodeDto.getSecretKey();
+
+        String cipherText = transform(plainText, secretKey, 1);
+
 
         log.info("cipherText:" + cipherText);
         log.info("------------------------");
@@ -57,11 +99,7 @@ public class DESServiceImpl implements DESService {
         String cipherText = decodeDto.getCipherText();
         String secretKey = decodeDto.getSecretKey();
 
-        String[] subKeys = desManager.secretKeyExpand(secretKey);
-        String subKey1 = subKeys[0];
-        String subKey2 = subKeys[1];
-
-        String plainText = des(cipherText, subKey2, subKey1);
+        String plainText = transform(cipherText, secretKey, 2);
 
         log.info("plainText:" + plainText);
         log.info("------------------------");
